ef177e95dc105c447c5557e3ed175bca686ca615
Uzair <uzairali19b@gmail.com>
2021-10-18 20:43:22 +0500

    update addBook function

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-const books = document.getElementById('main');
+const theBookName = document.querySelector('.name');
+const theBookAuthor = document.querySelector('.author');
+const submit = document.querySelector('.add');
+const books = document.querySelector('.book-list');
 
-books.innerHTML = 'Hello World!';
+let bookList = [];
+
+function addBook(bookName, bookAuthor, bookid) {
+  const nameInput = document.createElement('li');
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'Remove';
+  removeButton.setAttribute('id', bookid);
+  if (bookName != undefined && bookAuthor != undefined) {
+    nameInput.append(bookName + ' by ' + bookAuthor + ' ');
+  } else {
+    nameInput.append(theBookName.value + ' by ' + theBookAuthor.value + ' ');
+  }
+  nameInput.appendChild(removeButton);
+  books.appendChild(nameInput);
+  removeButton.addEventListener('click', () => {
+    removeButton.parentElement.remove();
+    for (let i = 0; i < bookList.length; i++) {
+      const books1 = bookList[i].id;
+      if (books1 === bookid) {
+        bookList.splice(i, 1);
+        localStorage.setItem('books', JSON.stringify(bookList));
+      }
+    }
+  });
+}
