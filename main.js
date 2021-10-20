851a5aa3e465d25fdcecb4547c456b21aff95dbf
Uzair <uzairali19b@gmail.com>
2021-10-20 14:00:21 +0500

    initialize constructor

--- main.js
+++ main.js
@@ -1,58 +1,20 @@
-const theBookName = document.querySelector('.name');
-const theBookAuthor = document.querySelector('.author');
-const submit = document.querySelector('.add');
-const books = document.querySelector('.book-list');
-
-let bookList = [];
-
-function addBook(bookName, bookAuthor, bookid) {
-  const nameInput = document.createElement('li');
-  const removeButton = document.createElement('button');
-  removeButton.innerText = 'Remove';
-  removeButton.setAttribute('id', bookid);
-  if (bookName !== undefined && bookAuthor !== undefined) {
-    nameInput.append(bookName + ' by ' + bookAuthor + ' ');
-  } else {
-    nameInput.append(theBookName.value + ' by ' + theBookAuthor.value + ' ');
+class BookList {
+  constructor(bookName, bookAuthor, bookId, books, submit, bookList) {
+    this.bookName = bookName;
+    this.bookAuthor = bookAuthor;
+    this.bookId = bookId;
+    this.books = books;
+    this.submit = submit;
+    this.bookList = bookList;
   }
-  nameInput.appendChild(removeButton);
-  books.appendChild(nameInput);
-  removeButton.addEventListener('click', () => {
-    removeButton.parentElement.remove();
-    for (let i = 0; i < bookList.length; i++) {
-      const books1 = bookList[i].id;
-      if (books1 === bookid) {
-        bookList.splice(i, 1);
-        localStorage.setItem('books', JSON.stringify(bookList));
-      }
-    }
-  });
 }
 
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
-  const variable = {
-    name: theBookName.value,
-    author: theBookAuthor.value,
-    id: Math.random()
-      .toString(36)
-      .replace(/[^a-z]+/g, '')
-      .substr(0, 5),
-  };
-  addBook(undefined, undefined, variable.id);
-  bookList.push(variable);
-  localStorage.setItem('books', JSON.stringify(bookList));
-  localStorage.getItem('books');
-});
+const bookName = document.querySelector('.name');
+const bookAuthor = document.querySelector('.author');
+const books = document.querySelector('.book-list');
+const submit = document.querySelector('.add');
+const bookList = [];
+const booksLists = new BookList(bookName, bookAuthor, null, books, submit, bookList);
 
-window.addEventListener('load', () => {
-  if (localStorage.getItem('books') !== null) {
-    bookList = JSON.parse(localStorage.getItem('books'));
-    bookList.forEach((book) => {
-      const booksNames = book.name;
-      const booksAuthors = book.author;
-      const booksid = book.id;
-      addBook(booksNames, booksAuthors, booksid);
-    });
-  }
-});
+booksLists.submitBook();
+booksLists.localStorage();
